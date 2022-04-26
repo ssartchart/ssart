bd5ec2aa8f8065513d66325ab9d1ff7af2710f22
배용한 <byh5112@naver.com>
2022-04-26 02:05:42 +0000

    Update function.js

--- src/function.js
+++ src/function.js
@@ -1,3 +1,38 @@
-// 기능 추가
+import {BarChart} from './BarChart.js'
 
-// 기능 추가 2
+
+function Chart2(type,id,labels,dataset,width,height,margin,padding,y_max = -1, y_min= 0){
+
+    const svg = d3.select(id).append('svg').style('width', width).style('height', height);
+    // const tooltip = d3.select(id).append('div').attr('id', 'tooltip');
+    console.log(`Hello, ${type}!`);
+
+    if (y_min == -1){
+        y_min = dataset[0].data[0].value;
+        dataset.forEach( data => {
+            data.data.forEach(d => {
+                if (d.value < y_min){
+                    y_min = d.value;
+                }
+            });
+        });
+    }
+    if (y_max == -1){
+        y_max = dataset[0].data[0].value;
+        dataset.forEach( data => {
+            data.data.forEach(d => {
+                if (d.value > y_max){
+                    y_max = d.value;
+                }
+            });
+        });
+    }
+
+    if (type==="bar"){
+        BarChart(svg,labels,dataset,width,height,margin,padding,y_max,y_min);
+    }
+
+};
+
+
+export {Chart};
