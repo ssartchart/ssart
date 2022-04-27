571221f7ad54b0bf95e49a7bed293db6a15c5f66
조원빈 <jwb7214@naver.com>
2022-04-27 16:48:45 +0900

    📝 Feat: [S06P31S201-60] 최소 margin 모듈 추가

--- src/function.js
+++ src/function.js
@@ -1,38 +1,84 @@
-import {BarChart} from './BarChart.js'
+import { BarChart } from "./BarChart.js";
+import { drawTitle, drawXTitle, drawYTitle } from "./title.js";
+import { checkMargin } from "./checkMargin.js";
 
+function Chart(
+  type,
+  id,
+  labels,
+  dataset,
+  width,
+  height,
+  margin,
+  padding,
+  y_max = -1,
+  y_min = 0
+) {
+  const svg = d3
+    .select(id)
+    .append("svg")
+    .style("width", width)
+    .style("height", height);
+  // const tooltip = d3.select(id).append('div').attr('id', 'tooltip');
+  console.log(`Hello, ${type}!`);
 
-function Chart(type,id,labels,dataset,width,height,margin,padding,y_max = -1, y_min= 0){
+  if (y_min == -1) {
+    y_min = dataset[0].data[0].value;
+    dataset.forEach((data) => {
+      data.data.forEach((d) => {
+        if (d.value < y_min) {
+          y_min = d.value;
+        }
+      });
+    });
+  }
 
-    const svg = d3.select(id).append('svg').style('width', width).style('height', height);
-    // const tooltip = d3.select(id).append('div').attr('id', 'tooltip');
-    console.log(`Hello, ${type}!`);
+  checkMargin(margin);
 
-    if (y_min == -1){
-        y_min = dataset[0].data[0].value;
-        dataset.forEach( data => {
-            data.data.forEach(d => {
-                if (d.value < y_min){
-                    y_min = d.value;
-                }
-            });
-        });
+  if (y_max == -1) {
+    y_max = dataset[0].data[0].value;
+    dataset.forEach((data) => {
+      data.data.forEach((d) => {
+        if (d.value > y_max) {
+          y_max = d.value;
+        }
+      });
+    });
+  }
+
+  if (type === "bar") {
+    BarChart(
+      svg,
+      labels,
+      dataset,
+      width,
+      height,
+      margin,
+      padding,
+      y_max,
+      y_min
+    );
+  }
+
+  // except circle
+  if (type != "circle") {
+    if (options.plugins.title.display) {
+      drawTitle(svg, options.plugins.title.text, width, height, margin);
     }
-    if (y_max == -1){
-        y_max = dataset[0].data[0].value;
-        dataset.forEach( data => {
-            data.data.forEach(d => {
-                if (d.value > y_max){
-                    y_max = d.value;
-                }
-            });
-        });
+    if (options.plugins.xTitle.display) {
+      drawXTitle(svg, options.plugins.xTitle.text, width, height, margin);
     }
-
-    if (type==="bar"){
-        BarChart(svg,labels,dataset,width,height,margin,padding,y_max,y_min);
+    if (options.plugins.yTitle.display) {
+      drawYTitle(
+        svg,
+        options.plugins.yTitle.text,
+        width,
+        height,
+        margin,
+        options.plugins.yTitle.position
+      );
     }
+  }
+}
 
-};
-
-
-export {Chart};
+export { Chart };
--- src/index.js
+++ src/index.js
@@ -1,3 +1,5 @@
 export { Chart } from "./function.js";
 
 export { drawTitle, drawXTitle, drawYTitle } from "./title.js";
+
+export { checkMargin } from "./checkMargin.js";
