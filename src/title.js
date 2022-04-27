ef7a8f2129305c8069df2ca637cd2d7bdd887a37
조원빈 <jwb7214@naver.com>
2022-04-27 14:20:24 +0900

    ✏️ Style: [S06P31S201-57] 기본 스타일 적용 해제

--- src/title.js
+++ src/title.js
@@ -5,7 +5,7 @@ function drawTitle(svg, text, width, height, margin) {
     .attr("x", width / 2)
     .attr("y", margin.top / 2)
     .attr("text-anchor", "middle")
-    .style("font-size", "30px")
+    .style("font-size", "16px")
     .style("font-weight", "bold")
     .style("font-family", "sans-serif")
     .text(text);
@@ -20,32 +20,26 @@ function drawXTitle(svg, text, width, height, margin) {
       "translate(" + width / 2 + " ," + (height - margin.bottom / 4) + ")"
     )
     .attr("text-anchor", "middle")
-    .style("font-size", "20px")
-    .style("font-family", "sans-serif")
     .text(text);
 }
 
 // y title
 function drawYTitle(svg, text, width, height, margin, position) {
-  if (position === "left") {
+  if (position === "right") {
     svg
       .append("text")
-      .attr("transform", "rotate(-90)")
-      .attr("x", -(height / 2))
-      .attr("y", margin.left / 2)
+      .attr("x", width - margin.right / 3)
+      .attr("y", height / 2)
       .attr("text-anchor", "middle")
-      .style("font-size", "20px")
-      .style("font-family", "sans-serif")
+      .attr("writing-mode", "tb")
       .text(text);
   } else {
     svg
       .append("text")
-      .attr("x", width - margin.right / 2)
-      .attr("y", height / 2)
+      .attr("transform", "rotate(-90)")
+      .attr("x", -(height / 2))
+      .attr("y", margin.left / 3)
       .attr("text-anchor", "middle")
-      .attr("writing-mode", "tb")
-      .style("font-size", "20px")
-      .style("font-family", "sans-serif")
       .text(text);
   }
 }
