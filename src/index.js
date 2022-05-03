c13c58a08942e4d5ac7c51a481f5ad5396173bf3
조원빈 <jwb7214@naver.com>
2022-05-03 13:36:21 +0900

    📝 Feat : [S06P31S201-91] 원형차트 모듈 추가

--- src/index.js
+++ src/index.js
@@ -1,6 +1,7 @@
-export {Chart, ChartH} from './function.js';
+export { Chart, ChartH } from "./function.js";
 
 export { drawTitle, drawXTitle, drawYTitle } from "./title.js";
 
 export { checkMargin } from "./checkMargin.js";
 
+export { drawCircleChart } from "./circleChart.js";
