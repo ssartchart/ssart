3457880e459b50086163d4d00beddc9a2e6f7b48
정해윤 <godbs98@naver.com>
2022-05-04 15:48:01 +0900

    📝 Feat : [S06P31S201-100] radar 차트 추가

--- src/RadarChart.js
+++ src/RadarChart.js
@@ -1,92 +1,100 @@
-// export class Radarchart{
-//     constructor({chart_area,labels,datasets,color,width,height,margin,padding,y_max,y_min}){
-//         const x_domain = labels.map(d => d);        
-//         const y_domain = [y_min,  (y_max != null) ? y_max : d3.max(datasets, label=>{
-//                 return d3.max(label.data, d=>{
-//                     return d.y;});            
-//                 })];        
-//         const Axis = Set_Axis({chart_area,x_domain,y_domain,width,height,margin,padding,x_type:"number"});
+// export class RadarChart {
+//     constructor({type,svg,width,height}){
 
-
-//         this.color = color;
-//         this.y_min = y_min;
-//         this.x = Axis.x;
-//         this.y = Axis.y;
-
-//         console.log(datasets);
-
-//         this.ChartBody = chart_area.selectAll(".slice")
-//             .data(datasets)
-//             .enter().append("g")
-//             .attr("class", "chartBody")
-//             .attr("transform", "translate(" + 0 + "," + 0 + ")")
-
-
-
-//         this.ChartBody.selectAll(".data")
-//             .data(datasets=>{return datasets.data;})
-//             .enter().append("circle")
-//             .filter(d=>{return this.x(d.x)>= 0 && this.x(d.x) <= width - margin.left && this.y(d.y)>= 0 && this.y(d.y) <= height - margin.top;})
-//             .attr("class","data")
-//             .attr("x",  d=> { return this.x(d.x); } )
-//             .attr("y", d=> { return this.y(d.y); } )
-//             .attr("transform", (d)=>{
-//                 return "translate(" + this.x(d.x) + "," + this.y(d.y) + ")";
-//             })
-//             .attr("r", 5)
-//             .style("fill",d=>{return this.color(d.label_index);})
-//             .style("fill-opacity", 1)
-
-//         chart_area.node();
-        
 //     }
+
 
 // }
 
+
 function MakeRadar(){
-    const svgDimensions = {
-        width: 300, height: 300,
-    };
-    // const radius = Math.min(svgDimensions.width, svgDimensions.height) / 2; // 반지름.
-    const data = [1,2,3,4,5];
-    // const num = 5;
-    // const points = [
-    //     -130,-40 ,
-    //     0,-150 ,
-    //     130,-40 ,
-    //     80,140 ,
-    //     -80,140 ,
-    //    ]
 
-    const svg = d3 // body
-        .select("body")
-        .append("svg")
-        .attr("width", svgDimensions.width)
-        .attr("height", svgDimensions.height)
-        .style("border", "3px solid rgba(0,0,0,0.1)")
+    const data = [];
+    const features = ["A","B","C","D"];
+    for (var i = 0; i < 3; i++){
+    var point = {}
+    features.forEach(f => point[f] = 1 + Math.random() * 10);
+    data.push(point);
+    }
 
-    const g = svg
-        .append("g")
-        .attr(
-        "transform",
-        `translate(${svgDimensions.width /2}, ${svgDimensions.height / 2})`
-        );
+    const svg = d3.select("body").append("svg")
+    .attr("width", 600)
+    .attr("height", 600);
 
-    // const color = d3.scaleOrdinal([ //  색상코드
-    //     "#64a0ff",
-    // ]);
+    const radialScale = d3.scaleLinear()
+    .domain([0,30]) // 시각화 데이터 대상 데이터 min max
+    .range([0,500]); // 출력 결과 min max
 
-    // const arc = d3.arc().innerRadius(40).outerRadius(radius); // 파이 모양 결정 (내구 반지름 /외부 반지름)
-    const poly = g
-        .selectAll("path")
-        .data(data)
-        .enter()
-        .append('path')
-        .attr('d',"M -130,-40 0,-150 130,-40 80,140 -80,140 Z M -105,-30 ,0,-125 105,-30 , 65,115 , -65,115 Z M -80,-20 ,0,-100 80,-20 , 52,95 , -52,95 Z M -55,-10 ,0,-70 55,-10 , 37,70 , -37,70 Z  M -30,0 ,0,-40 30,0 , 18,40 , -18,40 Z     M 0,10 0,-150 , 0,10 , -130,-40 , 0 ,10 , 130,-40 ,0,10 , 80,140,0,10,-80,140")
-        .attr("fill", "none")
-        .style('stroke',"black")
-        .style('stroke-width', '0.1px')
+    
+    const ticks = [2,4,6,8,10]; //그래프간 거리 .
+
+    ticks.forEach(t =>
+    svg.append("circle")
+    .attr("cx", 300)
+    .attr("cy", 300)
+    .attr("fill", "none")
+    .attr("stroke", "black")
+    .attr("r", radialScale(t))
+    );  // 원 축 그리는 기능.
+
+    function angleToCoordinate(angle, value){ // 중간 좌표 return 함수.
+        const x = Math.cos(angle) * radialScale(value);
+        const y = Math.sin(angle) * radialScale(value);
+        return {"x": 300 + x, "y": 300 - y};
+    }
+
+    for (var i = 0; i < features.length; i++) { // 축 갯수에 따른 세분화
+    const ft_name = features[i]; 
+    const angle = (Math.PI / 2) + (2 * Math.PI * i / features.length); //각도
+    const line_coordinate = angleToCoordinate(angle, 10); // 라인
+    const label_coordinate = angleToCoordinate(angle, 11); // 축 이름
+
+    svg.append("line") // 십자선
+    .attr("x1", 300)
+    .attr("y1", 300) // 중점 
+    .attr("x2", line_coordinate.x)
+    .attr("y2", line_coordinate.y)
+    .attr("stroke","black");
+
+    svg.append("text") // 축 이름 라벨링
+    .attr("x", label_coordinate.x)
+    .attr("y", label_coordinate.y)
+    .text(ft_name);
+    }
+
+    const line = d3.line()
+    .x(d => d.x)
+    .y(d => d.y);
+    
+    const colors = ["red", "blue", "green"]; // 폴리곤 색상.
+
+    function getPathCoordinates(data_point){
+        const coordinates = [];
+        for (var i = 0; i < features.length; i++){
+            const ft_name = features[i];
+            const angle = (Math.PI / 2) + (2 * Math.PI * i / features.length);
+            coordinates.push(angleToCoordinate(angle, data_point[ft_name]));
+        }
+        return coordinates;
+    }
+    
+    for (var i = 0; i < data.length; i ++){
         
+        const d = data[i];
+        const color = colors[i];
+        const coordinates = getPathCoordinates(d);
+
+        //draw the path element
+        svg.append("path")
+        .datum(coordinates)
+        .attr("d",line)
+        .attr("stroke-width", 3)
+        .attr("stroke", color)
+        .attr("fill", color)
+        .attr("stroke-opacity", 1)
+        .attr("opacity", 0.5);
+    }
+
+console.log("레이더: " + data);
 
 }
-// export{PieChart} 
