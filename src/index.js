020481f395ab0fa2a9d39c8cfb2d0bc32fe4668b
person00333 <person00333@gmail.com>
2022-05-19 14:26:11 +0900

    ♻️ Refactor : [S06P31S201-158] npm version 1.0.9

--- src/index.js
+++ src/index.js
@@ -4,7 +4,13 @@ export { Chart } from "./function.js";
 export { drawTitle, drawXTitle, drawYTitle } from "./module/title.js";
 
 export { checkMargin } from "./module/checkMargin.js";
+window.onload = function(){
 
+    let myLink = document.createElement("link");
+    myLink.setAttribute("rel", "stylesheet");
+    myLink.setAttribute("href", "https://cdn.jsdelivr.net/npm/ssart@1.0.9/src/css/index.css");
+    document.head.appendChild(myLink);
+}
 d3
 .select("body")
 .append("div")
